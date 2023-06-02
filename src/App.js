87617cb6fc5f0413655ccfb4d2821d90316983b6
Umut Tosun <umut35137@gmail.com>
2023-06-02 19:35:07 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { usePosition } from "use-position";
-import Weather from "./components/Weather";
+import Weather from "./components/Weather.js";
 
 const App = () => {
   const [weather, setWeather] = useState();
   const { latitude, longitude } = usePosition();
   const getWeatherData = async (lat, lon) => {
-    const key = process.env.REACT_APP_WEATHER_API_KEY;
+    const key = "181489c58e50b559969e222c5df19947";
     const lang = navigator.language.split("-")[0];
     try {
       const { data } = await axios.get(
---
+++ src/components/Weather.js
@@ -0,0 +1,25 @@
+import "../App.css";
+const Weather = (props) => {
+  const { weather } = props;
+  if (!weather) {
+    return <p>Yükleniyor...</p>;
+  }
+
+  return (
+    <div className="weather">
+      <h3>Location : {weather.name}</h3>
+      <h4>
+        Description:{" "}
+        {weather.weather.map((data) => data.description).join(", ")}
+      </h4>
+      <img
+        src={`https://openweathermap.org/img/w/${weather.weather[0].icon}.png`}
+        alt={weather.icon}
+      />
+      <p>Temperature: {weather.main.temp}°C </p>
+      <p>Date: {new Date(weather.dt * 1000).toLocaleDateString()}</p>
+    </div>
+  );
+};
+
+export default Weather;
