ccf2661a33e54bb90490f5b223d9899d5563c707
Umut Tosun <umut35137@gmail.com>
2023-06-03 14:54:05 +0300

    Fix :api request exported

--- src/App.js
+++ src/App.js
@@ -1,33 +1,28 @@
-// https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-import axios from "axios";
 import { useEffect, useState } from "react";
 import { usePosition } from "use-position";
-import Weather from "./components/Weather.js";
+
+import { Weather } from "./components";
+
+import { getWeatherData } from "./services/index.js";
 
 const App = () => {
   const [weather, setWeather] = useState();
   const { latitude, longitude } = usePosition();
-  const getWeatherData = async (lat, lon) => {
-    const key = "181489c58e50b559969e222c5df19947";
-    const lang = navigator.language.split("-")[0];
-    try {
-      const { data } = await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&${lang}&units=metric`
-      );
-      setWeather(data);
-    } catch {
-      alert("Hata oluştu");
-    }
+
+  const getWeather = async () => {
+    const data = await getWeatherData(latitude, longitude);
+    setWeather(data);
   };
 
   useEffect(() => {
-    latitude && longitude && getWeatherData(latitude, longitude);
+    latitude && longitude && getWeather();
   }, [latitude, longitude]);
+
   return (
-    <div>
+    <>
       <h2 className="weather">Weather</h2>
       <Weather weather={weather} />
-    </div>
+    </>
   );
 };
 export default App;
